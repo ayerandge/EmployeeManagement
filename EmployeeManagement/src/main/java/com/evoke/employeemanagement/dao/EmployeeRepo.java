d10750281db5ae28b4157d763bb0648c6c09aafe
ayerandge <abishek.y93@gmail.com>
2021-12-01 16:09:42 +0530

    Removed custome queries from Repo class

--- EmployeeManagement/src/main/java/com/evoke/employeemanagement/dao/EmployeeRepo.java
+++ EmployeeManagement/src/main/java/com/evoke/employeemanagement/dao/EmployeeRepo.java
@@ -1,9 +1,6 @@
 package com.evoke.employeemanagement.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.evoke.employeemanagement.entity.Employee;
@@ -11,10 +8,4 @@ import com.evoke.employeemanagement.entity.Employee;
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 	
-	@Query(value="select *  from employee e",nativeQuery = true)
-	List<Employee> findallTheEmployees();
-
-	@Query(value="select e from Employee e where e.id= :id")
-	Employee findEmployeeById(Long id);
-
 }
