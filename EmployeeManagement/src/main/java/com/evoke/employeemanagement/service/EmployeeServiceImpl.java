92f1bb040d0082de7b4dee5195f24907aa51949c
ayerandge <abishek.y93@gmail.com>
2021-12-09 19:44:21 +0530

    updated ehcache file to store at provided classpath

--- EmployeeManagement/src/main/java/com/evoke/employeemanagement/service/EmployeeServiceImpl.java
+++ EmployeeManagement/src/main/java/com/evoke/employeemanagement/service/EmployeeServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.evoke.employeemanagement.DTO.EmployeeDTO;
 import com.evoke.employeemanagement.dao.EmployeeRepo;
